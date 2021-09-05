b1f9b1aec130ee38bcc822731cd7af41daf572aa
ahsamt <tarpachova@gmail.com>
2021-09-05 21:58:34 +0100

    plotly imported

--- stockscreener/views.py
+++ stockscreener/views.py
@@ -4,17 +4,45 @@ from django.shortcuts import render
 from django.urls import reverse
 from django.db import IntegrityError
 from .models import User
+from django.conf import settings
+from django import forms
+import os
+
+import plotly.express as px
 
 import pandas as pd
 import yfinance as yf
 
+class StockForm(forms.Form):
+    stock = forms.CharField(label = "Stock name", max_length=5)
+
+
 def index(request):
     if request.method == "GET":
+        stockForm = StockForm()
         ticker = ["AAPL", "BA", "KO", "IBM", "DIS", "MSFT"]
-        stocks = yf.download(ticker, start = "2021-01-01", end = "2021-08-31")
-        stocks.to_csv("stocks.csv") 
-        stocks = pd.read_csv("stocks.csv", header = [0, 1], index_col = [0], parse_dates = [0])
-        return render(request, "stockscreener/index.html")
+        
+        return render(request, "stockscreener/index.html", {"stockForm": stockForm})
+   
+    if request.method == "POST":
+        if 'stock' in request.POST:
+            stockForm = StockForm(request.POST) 
+            if stockForm.is_valid():
+                stock = stockForm.cleaned_data['stock']
+                print(stock)
+                ticker=[stock]
+                
+                stocks = yf.download(ticker, start = "2010-01-01", end = "2021-08-31")
+                stocks.to_csv("stocks.csv")
+                stocks = pd.read_csv("stocks.csv", header = [0], index_col = [0], parse_dates = [0])
+                a = stocks.reset_index()
+                close = stocks.loc[:, "Close"].copy()
+
+                
+
+                fig = px.bar(a, x="Date", y="Close", barmode="group")
+                
+        return render(request, "stockscreener/index.html", {"stockForm": stockForm, "fig":fig})
 
 def login_view(request):
     if request.method == "POST":
