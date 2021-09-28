29113e29db40ebfba4c3a78d537e32c6dd9d1210
ahsamt <tarpachova@gmail.com>
2021-09-28 16:26:22 +0100

    docstrings added to utils.py

--- stockscreener/utils.py
+++ stockscreener/utils.py
@@ -12,60 +12,81 @@ buttons = [{"count": 5, "label": "5D", "step": "day","stepmode": "backward"},
 
 
 def get_SP_500_dict():
+    """gets the latest S&P 500 list from Wikipedia 
+    and returns it as a dictionary in a format {"ticker":"company name"}
+    """
     table=pd.read_html('https://en.wikipedia.org/wiki/List_of_S%26P_500_companies')
     df = table[0]
     dictSP500 = pd.Series(df.Security.values,index=df.Symbol).to_dict()
     return dictSP500
 
 def moving_average(series, window_size):
-    
+    """(pd Series, integer) -> pd series 
+    Calculates moving average for the given window size (in days).  
+    """ 
     windows = series.rolling(window_size)
-    moving_avgs = windows.mean()
-    return moving_avgs
+    movingAvgs = windows.mean()
+    return movingAvgs
 
-def prep_graph_data(stock):
+def prep_graph_data(stock, numMonths = 12):
+    """(string) => (pd DataFrame, pd DataFrame)
+    Takes in the stock ticker, returns two Pandas DataFrames for the period of time specified:
+    1st df - stock prices with 20 days and 50 days moving averages;
+    2nd df - normalised stock and S&P 500 prices. 
+    """
     ticker = [stock, "^GSPC"]
-    num_months = 12
-    end_date = date.today()
-                
-    start_date = end_date + relativedelta(months = -num_months)
-    start_date_internal = start_date + relativedelta(months= -3) 
-    start_date_datetime = datetime.combine(start_date, datetime.min.time()) 
-    # stocks = yf.download(ticker, start = start_date_internal, end = end_date)
-    # stocks.to_csv("stocks.csv")
+    endDate = date.today()              
+    startDate = endDate + relativedelta(months = -numMonths)
+    startDateInternal = startDate + relativedelta(months= -3) 
+    startDateDatetime = datetime.combine(startDate, datetime.min.time()) 
+    stocks = yf.download(ticker, start = startDateInternal, end = endDate)
+    stocks.to_csv("stocks.csv")
     stocks = pd.read_csv("stocks.csv", header = [0, 1], index_col = [0], parse_dates = [0])
     stocks.columns = stocks.columns.to_flat_index()
     stocks.columns = pd.MultiIndex.from_tuples(stocks.columns)
     stocks.swaplevel(axis = 1).sort_index(axis = 1)
-    close = stocks.loc[:, "Close"].copy()
 
-    close["moving_avg_20"] = moving_average(close[stock].copy(), 20)
-    close["moving_avg_50"] = moving_average(close[stock].copy(), 50)
+    closeData = stocks.loc[:, "Close"].copy()
+    closeData["moving_avg_20"] = moving_average(closeData[stock].copy(), 20)
+    closeData["moving_avg_50"] = moving_average(closeData[stock].copy(), 50)
 
-    mask = (close.index >= start_date_datetime)
-    close_spec = close.loc[mask]
-    norm = close_spec.div(close_spec.iloc[0]).mul(100)
-    norm = norm.reset_index()
-    close_spec = close_spec.reset_index()
-    close_spec.to_csv("close.csv")
+    mask = (closeData.index >= startDateDatetime)
+    closeDataForGraph = closeData.loc[mask]
+    normSPData = closeDataForGraph.div(closeDataForGraph.iloc[0]).mul(100)
+    normSPData = normSPData.reset_index()
 
-    return(close_spec, norm)
+    closeDataForGraph = closeDataForGraph.reset_index()
+
+    return(closeDataForGraph, normSPData)
 
 def get_change_info(data, stock):
-    closing_price = data[stock].iloc[-1]
-    previous_price = data[stock].iloc[-2]
-    price_dif = closing_price - previous_price
-    perc_dif = round(price_dif/previous_price*100, 2) 
-    if price_dif > 0:
+    """(pd DataFrame, string) => (float, tuple)
+    Takes in Pandas DataFrame with stock prices and a stock ticker, 
+    returns:
+    1 - a float for the last closing price 
+    2 - a tuple:
+        - a string showing the changes in price since previous trading day,
+        - a colour name (red or green) to use in HTML template to display the change.
+    """
+    closingPrice = data[stock].iloc[-1]
+    previousPrice = data[stock].iloc[-2]
+    priceDif = closingPrice - previousPrice
+    percDif = round(priceDif/previousPrice*100, 2) 
+    if priceDif > 0:
         sign = "+"
         color = "green"
     else:
         sign = ""
         color = "red"
-    change = (f"{sign}${round(price_dif,2)}  ({sign}{perc_dif}%)", color)
-    return (round(closing_price,2), change)   
+    change = (f"{sign}${round(priceDif,2)}  ({sign}{percDif}%)", color)
+    return (round(closingPrice,2), change)   
 
 def make_graph_1(data, stock, height, width):
+    """(pd DataFrame, string, integer, integer) => string
+    Takes in Pandas DataFrame with stock prices and moving averages,
+    stock ticker, desired height and width of the graph. 
+    Returns an HTML string representing the graph. 
+    """
     fig = go.Figure()
     fig.add_trace(go.Scatter(name=stock, x=data["Date"], y=data[stock])) 
     fig.add_trace(go.Scatter(name="20 day moving avg", x=data["Date"], y=data["moving_avg_20"])) 
@@ -75,14 +96,16 @@ def make_graph_1(data, stock, height, width):
             "rangeselector": {
                 "buttons": buttons
             }}, width = width, height=height 
-        )
-    
-    
-                
+        )     
     graph = fig.to_html(full_html=False)
     return graph
 
 def make_graph_2(data, stock, height, width):
+    """(pd DataFrame, string, integer, integer) => string
+    Takes in Pandas DataFrame with stock and S&P 500 prices,
+    stock ticker, desired height and width of the graph. 
+    Returns an HTML string representing the graph. 
+    """
     fig = go.Figure()
     fig.add_trace(go.Scatter(name=stock, x=data["Date"], y=data[stock]))
     fig.add_trace(go.Scatter(name="S&P 500", x=data["Date"], y=data["^GSPC"]))
