087c5ac5a3eed63351688a0073ca73af63edaef4
ahsamt <tarpachova@gmail.com>
2021-10-03 21:56:47 +0100

    settings updated

--- config/settings.py
+++ config/settings.py
@@ -131,4 +131,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # Heroku: Update database configuration from $DATABASE_URL.
 import dj_database_url
-db_from_env = dj_database_url.config(conn_max_age=500)
+db_from_env = dj_database_url.config(conn_max_age=500, ssl_require=True)
+DATABASES['default'].update(db_from_env)
